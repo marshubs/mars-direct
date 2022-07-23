d88c464c43f3fa6716331bab413aec9cdebcb40f
marshubs <99849732+marshubs@users.noreply.github.com>
2022-07-24 00:39:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
-/*
-MIT License
-
-Copyright (c) 2019 Iqbal Rifai
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 error_reporting(0);
 function get_http_response_code($redirect)
   {
@@ -35,7 +12,7 @@ function my_simple_crypt($string, $action = 'e')
     $output         = true;
     $encrypt_method = "AES-128-CBC";
     $key            = hash('sha1', $secret_key);
-    $iv             = substr(hash('sha1', $secret_iv), 0, 16);
+    $iv             = substr(hash('sha1', $secret_iv), 0, 5);
     if ($action == 'e')
       {
         $output = base64_encode(openssl_encrypt($string, $encrypt_method, $key, 0, $iv));
